c628a1fc1f1d3583f9e1dcc74f981b527fabf6e3
Will <doggy.huang@gmail.com>
2023-04-28 21:03:56 +0800

    修改 Program.cs 註冊 JwtBearer 服務到 DI 容器

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
