857d7bda531662d36c5c4a1187391c60408c607e
Will <doggy.huang@gmail.com>
2023-04-28 22:24:59 +0800

    設定只有 admin 與 manager 角色可以授權存取此 API

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Authorization;
 namespace AspNetCoreUserJwtsDemo.Controllers;
 
 [ApiController]
+[Authorize(Roles = "admin, manager")]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
